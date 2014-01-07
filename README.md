7bca5e42916e2be4274ad384ed5624b03ea2d24b
Norbert Budzyński <cross@reosh.co.uk>
2014-01-07 11:07:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+mc-kitchests
+============
+
+A Bukkit plugin that allows you to create chests with items that will respawn over time.
---
+++ src/main/java/uk/co/reosh/KitChests/KitChests.java
@@ -0,0 +1,39 @@
+/*
+ * KitChests Bukkit Plugin
+ * Copyright (C) 2014 REOSH Group <http://reosh.co.uk/> and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+package uk.co.reosh.KitChests;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KitChests extends JavaPlugin {
+	
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+	    this.saveDefaultConfig();
+		PluginDescriptionFile pdfile = this.getDescription();
+	}
+}
+		
+	
